2e003aaa6ad49fb64a245e25426635771dc37b01
FazidSamoon <tuan.fazid@outlook.com>
2023-04-08 11:09:52 +0530

    Update current-user.routes.ts

--- auth-service/src/routes/current-user.routes.ts
+++ auth-service/src/routes/current-user.routes.ts
@@ -3,7 +3,7 @@ import Express from "express";
 const router = Express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there sdfsf!");
+  res.send("Hi there!");
 });
 
 export { router as currentUserRouter };
